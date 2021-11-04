a52a2b2f70f6d6354143b7352fddfe506d26d135
nguyenminhuyen <66857062+nguyenminhuyen@users.noreply.github.com>
2021-11-04 23:04:00 +0700

    fix show initial tree

--- directory_tree_server.py
+++ directory_tree_server.py
@@ -7,6 +7,23 @@ from directory_tree_client import BUFFER_SIZE
 BUFSIZ = 1024 * 4
 SEPARATOR = "<SEPARATOR>"
 
+def showTree(sock):
+    listD = []
+    for c in range(ord('A'), ord('Z') + 1):
+        path = chr(c) + ":\\"
+        if os.path.isdir(path):
+            listD.append(path)
+    file_name = "dirs.pkl"
+    open_file = open(file_name, "wb")
+    pickle.dump(listD, open_file)
+    open_file.close()
+    file_size = os.path.getsize(file_name)
+    sock.sendall(str(file_size).encode())
+    temp = sock.recv(BUFSIZ)
+    with open(file_name, "rb") as f:
+        data = f.read()
+        sock.sendall(data)
+
 def sendListDirs(sock):
     path = sock.recv(BUFSIZ).decode()
     if not os.path.isdir(path):
@@ -87,6 +104,7 @@ def directory(client):
 
         if (mod == "SHOW"):
             client.sendall("OK".encode())
+            showTree(client)
             while True:
                 check = sendListDirs(client)
                 if not check[0]:    
