e5d1e140833a1a33032d3460976e04dd4e8a5552
LanAnh <lephamlananh@gmail.com>
2021-11-04 21:01:24 +0700

    Shutdown Logout

--- shutdown_logout_client.py
+++ shutdown_logout_client.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 import socket
 
+def close_event(main, client):
+    client.sendall(bytes("QUIT", "utf8"))
+    main.destroy()
+    return
+
 def shutdown(client):
     client.sendall(bytes("SHUTDOWN", "utf8"))
 def logout(client):
@@ -9,6 +14,8 @@ def logout(client):
 def shutdown_logout(client, root):
     window = tk.Toplevel(root)
     window.geometry("190x160")
+    window.grab_set()
+    window.protocol("WM_DELETE_WINDOW", lambda: close_event(window, client))
     shutdown_btn = tk.Button(window, text = 'SHUTDOWN', width = 20, height = 2, fg = 'white', bg = 'IndianRed3', command = lambda: shutdown(client), padx = 20, pady = 20)
     shutdown_btn.grid(row = 0, column = 0)
     logout_btn = tk.Button(window, text = 'LOGOUT', width = 20, height = 2, fg = 'white', bg = 'royalblue4', command = lambda: logout(client), padx = 20, pady = 20)
--- shutdown_logout_server.py
+++ shutdown_logout_server.py
@@ -2,10 +2,13 @@ import os, socket
 
 BUFSIZ = 1024 * 4
 def shutdown_logout(client):
-    msg = client.recv(BUFSIZ).decode("utf8")
-    if "SHUTDOWN" in msg:
-        os.system('shutdown -s -t 15')
-    elif "LOGOUT" in msg:
-        os.system('shutdown -l')
+    while(True):
+        msg = client.recv(BUFSIZ).decode("utf8")
+        if "SHUTDOWN" in msg:
+            os.system('shutdown -s -t 15')
+        elif "LOGOUT" in msg:
+            os.system('shutdown -l')
+        else:
+            return
     return
     
